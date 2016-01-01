549adbecd04f3e8b9fa8218055b68b4a69df742b
Mikael Strid <mikael.strid@outlook.com>
2016-01-01 21:39:53 +0100

    Fixade med Foundation, Sass och Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,35 @@ var gulp = require("gulp"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
   uglify = require("gulp-uglify"),
+  filter = require('gulp-filter'),
   project = require("./project.json"),
   sass = require('gulp-sass');
 
 var paths = {
-  webroot: "./wwwroot/"
+  webroot: "./wwwroot/",
+  bowerComponents: "./bower_components/"
 };
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
+paths.scss = "./scss/**/*.scss";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
+
+// === LIB ===
+gulp.task('lib:jquery', function () {
+   gulp.src(paths.bowerComponents + "jquery/dist/*.js")
+    .pipe(gulp.dest(paths.webroot + "lib/jquery"));
+});
+
+gulp.task("lib", ["lib:jquery"]);
+
+
+
+// === MIN ===
 gulp.task("clean:js", function(cb) {
   rimraf(paths.concatJsDest, cb);
 });
@@ -49,13 +64,19 @@ gulp.task("min:css", function() {
 gulp.task("min", ["min:js", "min:css"]);
 
 
-
-gulp.task('sass', function () {
-  gulp.src('./wwwroot/css/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./wwwroot/css'));
+// === SASS ===
+gulp.task("sass:site", function () {
+  gulp.src("./scss/site.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(paths.webroot + "css"));
+});
+gulp.task("sass:foundation", function () {
+  gulp.src("./scss/foundation-custom.scss")
+    .pipe(sass({includePaths: "bower_components/foundation/scss/"}).on("error", sass.logError))
+    .pipe(gulp.dest(paths.webroot + "css"));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./wwwroot/css/scss/**/*.scss', ['sass']);
+gulp.task("sass:watch", function () {
+  gulp.watch(paths.scss, ["sass:site"]);
+  gulp.watch(["./scss/foundation-custom.scss", "./scss/partials/_foundation-settings.scss"], ["sass:foundation"]);
 });
