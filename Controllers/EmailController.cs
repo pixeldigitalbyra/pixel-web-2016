174e0fba9ade6c17061ad52774acc2f73a8974bf
Mikael Strid <mikael.strid@outlook.com>
2016-02-02 22:53:26 +0100

    Fixade mejlskickning.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+#if DNX451
+    using System.Net;
+    using System.Net.Mail;
+    using System.Text;
+    using SendGrid;
+#endif
+
+namespace Pixel.Web.Controllers
+{
+    [Route("api/email")]
+    public class EmailController : Controller
+    {
+        // POST api/email
+        [HttpPost]
+        public async Task Post(string name, string contactInformation, string message)
+        {
+#if DNX451
+            var myMessage = new SendGridMessage();
+            myMessage.From = new MailAddress("noreply@pixeldigitalbyra.se");
+            myMessage.AddTo("info@pixeldigitalbyra.se");
+            myMessage.Subject = "Kontaktformulär på hemsidan";
+            myMessage.Html =
+                "<p>Namn:<br/>" + name + "</p>" +
+                "<p>Kontaktinformation:<br/>" + contactInformation + "</p>" +
+                "<p>Meddelande:<br/>" + message + "</p>";
+            var credentials = new NetworkCredential("azure_b634599bc566cf4a88301e4ce930013c@azure.com", "snDGrd19876!");
+            var transportWeb = new SendGrid.Web(credentials);
+            await transportWeb.DeliverAsync(myMessage);
+#else
+            throw new Exception("Sending emails not supported.");
+#endif
+        }
+    }
+}
--- wwwroot/js/base-page.js
+++ wwwroot/js/base-page.js
@@ -2,18 +2,33 @@ window.pixel = window.pixel || {};
 
 window.pixel.basePage = (function () {
     var init = function () {
-	    $(window).bind("scroll", function() {
-	        $("body").toggleClass("has-scrolled", $(document).scrollTop() > 0);
-	    });
+        $(window).bind("scroll", function () {
+            $("body").toggleClass("has-scrolled", $(document).scrollTop() > 0);
+        });
 
-	    $(".scroll-link").click(function () {
-	        var target = $(this).data("target");
-	        var duration = $(this).data("duration");
-	        var headerHeight = $("header").outerHeight();
-	        $("html, body").animate({
-	            scrollTop: $(target).offset().top - headerHeight
-	        }, duration);
-	    });
+        $(".scroll-link").click(function () {
+            var target = $(this).data("target");
+            var duration = $(this).data("duration");
+            var headerHeight = $("header").outerHeight();
+            $("html, body").animate({
+                scrollTop: $(target).offset().top - headerHeight
+            }, duration);
+        });
+
+        $("#btnSubmitContactForm").click(function () {
+            $.post("/api/email?" + 
+                "name=" + encodeURIComponent($("#txtName").val()) +
+                "&contactInformation=" + encodeURIComponent($("#txtContactInformation").val()) +
+                "&message=" + encodeURIComponent($("#txtMessage").val()))
+            .done(function () {
+                $("#contactForm").hide();
+                $("#contactSuccessMessage").removeClass("hide");
+            })
+            .fail(function () {
+                $("#contactFailMessage").removeClass("hide");
+            });
+            return false;
+        });
     }
 
     return {
